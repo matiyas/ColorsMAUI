16928dc675878109b91752e52bb4db9bc2bcd5ae
Damian Matyjaszek <tougepl@gmail.com>
2023-05-07 13:39:05 +0200

    Initial commit

---
+++ KolotyMAUI/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Xml;
+
+namespace KoloryMAUI;
+
+public partial class MainPage : ContentPage
+{
+    private bool _updateUI = true;
+
+	public MainPage()
+	{
+		InitializeComponent();
+        ReadSettings();
+	}
+
+    private void ReadSettings ()
+    {
+        var (r, g, b) = KoloryMAUI.Settings.Read();
+
+        _updateUI = false;
+        sliderR.Value = r;
+        sliderG.Value = g;
+        _updateUI = true;
+        sliderB.Value = b;
+    }
+
+    private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        if (!_updateUI) return;
+
+        UpdateRectangleColor();
+        UpdateSlidersLabels();
+    }
+
+    private void UpdateRectangleColor ()
+    {
+        var color = Color.FromRgb(sliderR.Value, sliderG.Value, sliderB.Value);
+        rectangle.Fill = new SolidColorBrush(color);
+    }
+
+    private void UpdateSlidersLabels ()
+    {
+        labelR.Text = GetSliderLabelText(sliderR.Value);
+        labelG.Text = GetSliderLabelText(sliderG.Value);
+        labelB.Text = GetSliderLabelText(sliderB.Value);
+    }
+
+    private static string GetSliderLabelText(double sliderValue)
+    {
+        return Math.Round(255 * sliderValue).ToString();
+    }
+
+    private void ContentPage_Disappearing(object sender, EventArgs e)
+    {
+        KoloryMAUI.Settings.Save(
+            sliderR.Value, 
+            sliderG.Value, 
+            sliderB.Value
+        );
+    }
+}
+
---
+++ KolotyMAUI/Settings.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using System.Xml.Linq;
+
+namespace KoloryMAUI;
+
+class Settings
+{
+    private static readonly string _filePath = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+        "colors.xml"
+    );
+    private static readonly IFormatProvider _formatProvider = CultureInfo.InvariantCulture;
+
+    public static void Save (double r, double g, double b)
+    {
+
+        XDocument xml = new(
+            new XComment($"Saved: {DateTime.Now}"),
+            new XElement("settings",
+                new XElement("r", r.ToString(_formatProvider)),
+                new XElement("g", g.ToString(_formatProvider)),
+                new XElement("b", b.ToString(_formatProvider))
+            )
+        );
+        xml.Save(_filePath);
+    }
+
+    public static (double r, double g, double b) Read ()
+    {
+        var defaultValue = (0, 0, 0);
+
+        if (!File.Exists(_filePath)) return defaultValue;
+
+        try
+        {
+            XDocument xml = XDocument.Load(_filePath);
+
+            double r = double.Parse(xml.Root.Element("r").Value, _formatProvider);
+            double g = double.Parse(xml.Root.Element("g").Value, _formatProvider);
+            double b = double.Parse(xml.Root.Element("b").Value, _formatProvider);
+
+            return (r, g, b);
+        }
+        catch
+        {
+            return defaultValue;
+        }
+    }
+}
